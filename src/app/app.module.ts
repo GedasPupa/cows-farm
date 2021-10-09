3ed1cadf12f47fe1c0e7728390ee9e4250702e10
GedasPupa <gedosas@gmail.com>
2021-10-09 09:53:38 +0300

    preparation stage+

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { CowsComponent } from './components/cows/cows.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CowComponent } from './components/cow/cow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CowsComponent,
+    FooterComponent,
+    CowComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: CowsComponent },
+      { path: 'cows', component: CowsComponent },
+      { path: 'add', component: CowComponent },
+      { path: '**', redirectTo: 'cows', pathMatch: 'full' },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
